e1edec29270dcda9e01d02e2f901af5125257071
minhhoang2705 <minhtranh@startupland.vn>
2024-05-08 16:31:49 +0700

    Refactor calculate_can_chi_calendar function to handle invalid year in main.py Add code to print name and person details in main.py Add code to print list and calculate sum in main.ipynb

--- 40-Day-Python/Day 1/main.py
+++ 40-Day-Python/Day 1/main.py
@@ -1,12 +1,45 @@
 def calculate_can_chi_calendar(year: int) -> str:
-    can = ["Canh", "Tân", "Nhâm", "Quý", "Giáp", "Ất", "Bính", "Đinh", "Mậu", "Kỷ"]
-    chi = ["Thân", "Dậu", "Tuất", "Hợi", "Tý", "Sửu", "Dần", "Mão", "Thìn", "Tị", "Ngọ", "Mùi"]
-    for i in range(0, len(can)):
-        for j in range(0, len(chi)):
-            if year % 10 == i and year % 12 == j:
-                result = can[i] + " " + chi[j]
-                return result
-    return result
+    if not isinstance(year, int):
+        raise TypeError("Year must be an integer.")
+
+    celestial_stems = [
+        "Canh",
+        "Tân",
+        "Nhâm",
+        "Quý",
+        "Giáp",
+        "Ất",
+        "Bính",
+        "Đinh",
+        "Mậu",
+        "Kỷ",
+    ]
+    terrestrial_branches = [
+        "Thân",
+        "Dậu",
+        "Tuất",
+        "Hợi",
+        "Tý",
+        "Sửu",
+        "Dần",
+        "Mão",
+        "Thìn",
+        "Tị",
+        "Ngọ",
+        "Mùi",
+    ]
+
+    stem_idx = year % 10
+    branch_idx = year % 12
+
+    if stem_idx < len(celestial_stems) and branch_idx < len(terrestrial_branches):
+        stem = celestial_stems[stem_idx]
+        branch = terrestrial_branches[branch_idx]
+        return f"{stem} {branch}"
+
+    # Return "Invalid year" if the year doesn't match any combination
+    return "Invalid year"
+
 
 if __name__ == "__main__":
     print(calculate_can_chi_calendar(2024))
---
+++ 40-Day-Python/Day 4/main.py
@@ -0,0 +1,9 @@
+# 1
+name = "Tommy"
+print(f"This is {name}")
+
+# 2 
+person = "Ms Taylor"
+print(f"Title case: {person.title()}")
+print(f"Upper case: {person.upper()}")
+print(f"Lower case: {person.lower()}")
